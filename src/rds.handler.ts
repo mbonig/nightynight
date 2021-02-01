a1c30f7b28b47b97ba1032d3c04f2d3bd119b843
Matthew Bonig <matthew.bonig@gmail.com>
2021-02-01 12:05:16 -0700

    fixing some perms and handler issues

--- src/rds.handler.ts
+++ src/rds.handler.ts
@@ -5,22 +5,20 @@ const dbInstanceIdentifier = process.env.DB_INSTANCE_IDENTIFIER;
 export const handler = async (event: any) => {
   console.log({ event });
   console.log({ dbInstanceIdentifier });
-
   console.log('Getting instance details');
-
   let params = {
     DBInstanceIdentifier: dbInstanceIdentifier,
   };
   const {
-    DBInstance: instanceDetails,
-  } = await rds.describeInstances(params).promise();
+    DBInstances: [instanceDetails],
+  } = await rds.describeDBInstances(params).promise();
   console.log({ instanceDetails });
   if (!instanceDetails) {
-    throw new Error('Couldn\'t find instance details. Weird... if the instance was terminated, this app should be too');
+    throw new Error("Couldn't find instance details. Weird... if the instance was terminated, this app should be too");
   }
   if (instanceDetails.DBInstanceStatus === 'available') {
     console.log('stopping the instance');
-    await rds.stopInstances(params).promise();
+    await rds.stopDBInstance({ DBInstanceIdentifier: dbInstanceIdentifier }).promise();
     let now = new Date().toISOString();
     console.log(`Stopped instance ${dbInstanceIdentifier} at ${now}`);
   } else {
